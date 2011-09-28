16ad9538429c7fd7598965b9acaabfdd8132fc0c
Quincy Tse <quincy.tse@sydney.edu.au>
2011-09-28 10:31:31 +1000

    Bugfix - incorrect use of urlencode, removed problematic "change unit" operation

--- util/scrapper.py
+++ util/scrapper.py
@@ -5,8 +5,8 @@ import urllib
 _logon_url = 'https://cadetone.aafc.org.au/logon.php'
 _logout_url = 'https://cadetone.aafc.org.au/logout.php'
 _change_unit_url = 'https://cadetone.aafc.org.au/changeunit.php'
-_act_details_url = 'https://cadetone.aafc.org.au/activities/viewactivity.php?ActID='
-_act_roll_url = 'https://cadetone.aafc.org.au/activities/nominalroll.php?ActID='
+_act_details_url = 'https://cadetone.aafc.org.au/activities/viewactivity.php?'
+_act_roll_url = 'https://cadetone.aafc.org.au/activities/nominalroll.php?'
 
 def _parse_act_pages(pages):
   return pages
@@ -23,26 +23,26 @@ def login(usr, pwd):
   opener.open(_logon_url, data=param)
 
   # Change to 3WG appointment
-  param = urllib.urlencode({
-      'UnitDetId' : '3,0'})
-  opener.open(_change_unit_url)
+###  param = urllib.urlencode({
+###      'UnitDetId' : '3,0'})
+###  opener.open(_change_unit_url)
 
   return opener
 
-def _logout(opener):
+def logout(opener):
   opener.open(_logout_url)
 
 def get_activity_info(act_ids, opener):
   pages = []
 
   for act in act_ids:
-    pages.append(opener.open(_act_details_url + urllib.urlencode(act)).content)
+    pages.append(opener.open(_act_details_url + urllib.urlencode({'ActID': act})).content)
 
   return _parse_act_pages(pages)
 
 def get_nominal_roll(act_ids, opener):
   pages = []
   for act in act_ids:
-    pages.append(opener.open(_act_roll_url + urllib.urlencode(act)).content)
+    pages.append(opener.open(_act_roll_url + urllib.urlencode({'ActID': act})).content)
 
   return _parse_roll_pages(pages)
